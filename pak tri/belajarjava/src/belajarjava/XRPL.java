132ffd8e94065491151ab3813ac6022d19b73a14
chaca0110 <ur.chacamhrani@gmail.com>
2026-01-15 02:48:52 -0800

    Add files via upload

---
+++ pak tri/belajarjava/src/belajarjava/XRPL.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package belajarjava;
+
+/**
+ *
+ * @author ICAH
+ */
+public class XRPL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+       int number;
+       number=10;
+       
+       System.out.println(number);
+       System.out.println("nomer yang anda inputkan: "+number);
+       number=100;
+        System.out.println("nomer terbaru adalah: "+number);
+       
+       
+        double desimal=10.355;
+       float pecahan=13.4555f;
+            System.out.println("desimal");
+            System.out.println("nomer pecahan yang diinputi: "+pecahan);
+            
+       String nama= "jack";
+       char abjad='A';
+            System.out.println("nama");
+            System.out.println("abjad");
+            
+       boolean betul= true;
+       boolean salah= false;
+            System.out.println("betul");
+            System.out.println("salah");
+            
+       //desimal= 100.235;
+            System.out.println("desimal");
+    }
+    
+}
+    
+    
