124e3bd6475542e00f1b11255d21aef9da8dab93
chaca0110 <ur.chacamhrani@gmail.com>
2026-01-25 18:23:06 -0800

    Add files via upload

---
+++ pak tri/belajarkonversidata.java
@@ -0,0 +1,19 @@
+
+package belajarjava;
+
+
+public class belajarkonversidata {
+
+    
+    public static void main(String[] args) {
+        System.out.println("belajar konversi data");
+        System.out.println("");
+        
+        String a= "25";
+        // int b= Integer.valueOf(a);
+        
+        int c= Integer.parseInt(a);
+        System.out.println(c+ 100);
+    }
+    
+}
